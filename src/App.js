ded1072770c71008b7996b620aeb45d4e8604e42
MongiBr <mongiberrima98@gmail.com>
2021-05-23 21:58:13 +0200

    front-orenda

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import "./App.css";
+
+import {BrowserRouter, Route , Switch} from 'react-router-dom'
+import Signin from "./components/authentification/signin";
+import Signup from "./components/authentification/signup";
+import Header from "./components/header/header"
+import { AuthContext, AuthProvider } from './context/authContext';
+import { useContext, useState } from 'react';
+import Home from "./pages/home";
+import Footer from './components/footer/footer'
 function App() {
+ const authContext=useContext(AuthContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+      <BrowserRouter>
+        <Header/>
+        <Switch>
+          <Route exact path='/'>
+           {authContext.auth.email? <Home/> : <Signin/>}
+          </Route>
+          <Route exact path='/signup'>
+            <Signup/>
+          </Route>
+        </Switch>
+        <Footer/>
+      </BrowserRouter>
+    </>
   );
 }
 
-export default App;
+
+function AppWithStore(){
+  return(
+     <AuthProvider>
+    <App/>
+  </AuthProvider>
+  )
+ 
+}
+
+export default AppWithStore;
